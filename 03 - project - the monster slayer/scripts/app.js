3ef8fa0ed1d42aeb1872906f14f793ca0442dc84
Siva Kumar <siva3378@gmail.com>
2019-05-13 12:13:35 +0200

    [01-project] : work in progress

---
+++ 03 - project - the monster slayer/scripts/app.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+    },
+    methods: {
+        startGame: function () {
+            this.gameIsRunning = true;
+        },
+        getDamage: function (max, min) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        attack: function () {
+            this.monsterHealth -= this.getDamage(10, 3);
+
+            if (this.monsterHealth <= 0) {
+                console.log('you won');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            if (this.playerHealth <= 0) {
+                console.log('you lose');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            this.playerHealth -= this.getDamage(12, 5);
+
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
+        }
+    }
+});
