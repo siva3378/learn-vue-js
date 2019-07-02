e9b284da37b6d9a9267985c39fd41ee1fc9a0e63
Siva Kumar <siva3378@gmail.com>
2019-07-02 12:36:52 +0200

    [ADDS] 006 assignment to understand more about vue components

---
+++ 01 - Assignments/006 - vue components/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <app-header />
+    <hr />
+    <app-servers />
+    <hr />
+    <app-footer />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ 01 - Assignments/006 - vue components/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import './components';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
